d1577f8ec174c3aa6bd818d86b3522b5f812cdac
Avuzwa Mdunyelwa <156984914+AvuMdunyelwa@users.noreply.github.com>
2025-04-07 04:57:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+// hamburger navigation
 const hamMenu = document.querySelector('#hamburger');
 const offScreenMenu = document.querySelector('#off-screen-menu');
 
@@ -6,12 +8,36 @@ hamMenu.addEventListener('click', () => {
     offScreenMenu.classList.toggle('active')
 } )
 
-const prgressBar = document.querySelector('#timer-numbers');
+// modals part
+// modal-1 (add timer)
 
-function enableProgressBar() {
-    prgressBar.setAttribute('role', 'progressBar');
-    prgressBar.setAttribute('arial-valuenow', 0)
-    prgressBar.setAttribute('arial-live', 'polite')
-}
+const modal1 = document.querySelector('#modal-1');
+const openModal1 = document.querySelector('#add-timer');
+const closeModal1 = document.querySelector('#open-modal-2');
+const closeModalbtn = document.querySelector('#close-modal-1');
+const modal2 = document.querySelector('#modal-2');
 
-enableProgressBar()
+const input = document.querySelectorAll('input');
+const inputAlert = document.querySelector('#alert')
+
+
+openModal1.addEventListener('click', () => {
+    modal1.show()
+})
+
+closeModalbtn.addEventListener('click', () => {
+
+        modal1.close()
+})
+
+closeModal1.addEventListener('click', () => {
+    
+    if(input.value === '') {
+        inputAlert.styles.display = 'block'
+
+    }else {
+        modal1.close();
+        modal2.show();
+
+    }
+})
